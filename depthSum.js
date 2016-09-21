90f0ef298607975ccfd03ee88d5fe3b57362b193
Nadine Ott <nadineott7@yahoo.com>
2016-09-20 17:02:41 -0700

    Nested...notes added

--- depthSum.js
+++ depthSum.js
@@ -1,4 +1,5 @@
 // Given the list [[1,1],2,[1,1]], return 10. (four 1's at depth 2, one 2 at depth 1)
+//Note- won't pass LeetCode specs, saying its returning NaN. Not sure where this is comign from every repl and console I run it in does fine.
 
 var depthSum = function(nestedList){
   var total = 0;
