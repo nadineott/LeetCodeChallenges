8c69ab8a979c07050ac5f6ba82b48f8d945b9d3b
Nadine Ott <nadineott7@yahoo.com>
2016-09-23 16:17:43 -0700

    Array middle description

--- arrayMiddle.js
+++ arrayMiddle.js
@@ -1,3 +1,7 @@
+// Given an array of negative/positive integers, return the element in the center position of the array.
+
+// If the array has an even number of elements, return the average of the two middle elements instead.
+
 function middle (numbers) {
   //if even
   if (numbers.length % 2 === 0) {
