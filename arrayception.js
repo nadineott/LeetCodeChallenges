f4fd394154efa4cf92268ce5e2317c0da8043094
Nadine Ott <nadineott7@yahoo.com>
2016-09-22 20:46:55 -0700

    Arrayception (easy)

--- arrayception.js
+++ arrayception.js
@@ -1,3 +1,7 @@
+// Given an array of arbitrarily nested arrays, return n, where n is the deepest level that contains a non-array value.
+//Input [ [ 5 ], [ [ ] ] ] output 2
+//Input [ [ 10, 20 ], [ [ 30, [ 40 ] ] ] ] Output 4
+//Input [ [ [ ] ] ] Output 0
 var arrayception = function(arr){
   var options = [0];
   var recurse = function(x, level) {
