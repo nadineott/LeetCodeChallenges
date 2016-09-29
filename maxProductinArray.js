07bc4c1a7f61450fc4769186d05bc3f61f2fc354
Nadine Ott <nadineott7@yahoo.com>
2016-09-28 17:15:30 -0700

    Max Product SubArray (medium)

---
+++ maxProductinArray.js
@@ -0,0 +1,18 @@
+// Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+// For example, given the array [2,3,-2,4],
+// the contiguous subarray [2,3] has the largest product = 6
+
+var maxProduct = function(nums) {
+    var max = Number.NEGATIVE_INFINITY;
+    for (var i = 0; i < nums.length; i++){
+      var total = 1;
+      for (var k = i; k < nums.length; k++){
+        total = total*nums[k];
+        max = Math.max(max, total);
+      }
+    }
+    return max;
+};
+
+console.log("expect 6", maxProduct([2,3,-2,4]));
