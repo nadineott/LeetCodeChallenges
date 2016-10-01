627ab6c11abf9267a71f7865257a15e6348ce76e
Nadine Ott <nadineott7@yahoo.com>
2016-09-30 18:46:13 -0700

    Connect The Dots (hard)

--- connectTheDots.js
+++ connectTheDots.js
@@ -18,47 +18,38 @@ var maxPoints = function(points) {
     return Math.max(horizontal, vertical, diagonal);
 };
 
+var mAndB = function(x, y, xx, yy){
+  var slope = (x-xx)/(y-yy);
+    var yInt = -1*((slope*x)-y);
+    var slopeInt = ""+slope+yInt;
+    return slopeInt;
+}
+
 var findDiagonals = function(arr){
-  console.log("DIAGONALS")
-  var storage = {};
   var max = 0;
-    for (var i = 0; i < arr.length; i++){
-      //console.log("new i ", i);
-      var firstX = arr[i][0];
-      //console.log("first X ", firstX)
-      var firstY = arr[i][1];
-      for (var j = 0; j < arr.length; j++){
+  var storage = {};
+  for (var i = 0; i < arr.length; i++){
+    var firstX = arr[i][0];
+    var firstY = arr[i][1];
+    for (var j = i+1; j < arr.length; j++){
+        if (firstX !== secondX && firstY !== secondY){
         var secondX = arr[j][0];
-        //console.log("secondX ", secondX)
         var secondY = arr[j][1];
-        if (i !== j && firstX !== secondX && firstY !== secondY){
-          //console.log("arr[i] ", arr[i], " arr[j] ", arr[j])
-          var slope = firstX-secondX/firstY-secondY;
-          //console.log("slope: ", slope)
-          var yInt = -1*((slope*firstX)-firstY);
-          //console.log("yInt ", yInt)
-          var slopeInt = ""+slope+yInt;
-          //console.log("slopeInt ", slopeInt)
-          //console.log("slopeInt: ", slopeInt)
-          if (storage[slopeInt] === undefined){
-            storage[slopeInt] = 1;
-          } else {
-            storage[slopeInt] = storage[slopeInt]+1;
-          }
-          //console.log("storage ", storage)
+        var slope = mAndB(firstX, firstY, secondX, secondY);
+        if (!storage[slope]){
+          storage[slope] = 1;
+        } else {
+          storage[slope] = storage[slope]+1;
         }
       }
     }
-    console.log("storage ", storage)
-    for (var key in storage){
-      if (storage[key] > max){
-        max = storage[key];
-      }
+  }
+  for (var key in storage){
+    if (storage[key] > max){
+      max = storage[key];
     }
-    return max;
-      //determine slope and y intercept that two points make
-      //create array of all points with same slope and y-int
-    //longest array of diagonals OR vertical/horizontal lines wins
+  }
+  return max;
 }
 
 var findLines = function(arr, num){
@@ -77,13 +68,76 @@ var findLines = function(arr, num){
       max = storage[key];
     }
   }
-  console.log(num, max)
+  return max;
+}var maxPoints = function(points) {
+    points = Array.prototype.slice.call(arguments);
+  //early return cases
+  if (points.length < 3){
+    if (points[0].length === 0){
+      return 0;
+    }
+    return points.length;
+  }
+    var horizontal = findLines(points, 0);
+    var vertical = findLines(points, 1);
+    //early return opportunity: since diagonal is going to be the most expensive function, if there is a horizontal or vertical line that is the max, no need to run diagonal
+    if (horizontal === points.length || vertical === points.length){
+      return horizontal;
+    }
+    var diagonal = findDiagonals(points);
+
+    return Math.max(horizontal, vertical, diagonal);
+};
+
+var mAndB = function(x, y, xx, yy){
+  var slope = (x-xx)/(y-yy);
+    var yInt = -1*((slope*x)-y);
+    var slopeInt = ""+slope+yInt;
+    return slopeInt;
+}
+
+var findDiagonals = function(arr){
+  var max = 0;
+  var storage = {};
+  for (var i = 0; i < arr.length; i++){
+    var firstX = arr[i][0];
+    var firstY = arr[i][1];
+    for (var j = i+1; j < arr.length; j++){
+        if (firstX !== secondX && firstY !== secondY){
+        var secondX = arr[j][0];
+        var secondY = arr[j][1];
+        var slope = mAndB(firstX, firstY, secondX, secondY);
+        if (!storage[slope]){
+          storage[slope] = 1;
+        } else {
+          storage[slope] = storage[slope]+1;
+        }
+      }
+    }
+  }
+  for (var key in storage){
+    if (storage[key] > max){
+      max = storage[key];
+    }
+  }
   return max;
 }
 
-// console.log("expect 3 ", maxPoints([0,1], [9,12], [0,2], [0,6], [9, 15]))
-// console.log("expect 0 ", maxPoints([]));
-// console.log("expect 1 ", maxPoints([2,4]))
-// console.log("expect 2 ", maxPoints([2,5], [3,7]))
-console.log("expect 5 (diagonal)", maxPoints([1,4], [2,3], [3,2], [4,1], [5,0]))
-// console.log("expect 3 (diagonal) ", maxPoints ([1,1], [2,2], [4,4], [3,13], [19,20]))
+var findLines = function(arr, num){
+  var storage = {};
+  var max = 0;
+  for (var i = 0; i < arr.length; i++){
+    var current = arr[i][num];
+    if (storage[current] === undefined){
+      storage[current] = 1;
+      } else {
+      storage[current] = storage[current]+1;
+      }
+  }
+  for (var key in storage){
+    if (storage[key] > max){
+      max = storage[key];
+    }
+  }
+  return max;
+}
