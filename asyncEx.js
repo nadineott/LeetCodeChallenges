0c3b7fb4e59ce62d8504c0afcaebe70b8b03b7d9
Nadine Ott <nadineott7@yahoo.com>
2016-10-11 09:18:39 -0700

    Async Execution test case (medium)

--- asyncEx.js
+++ asyncEx.js
@@ -13,3 +13,22 @@ var asyncMap = function(funcList, cb){
     })(i)
   }
 }
+
+//TESTg
+asyncMap([
+  function(cb){
+    setTimeout(function(){
+      cb('one');
+    }, 200);
+  },
+  function(cb){
+    setTimeout(function(){
+      cb('two');
+    }, 100);
+  }
+ ],
+  function(results){
+    // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+    console.log(results); // ['one', 'two']
+ });
