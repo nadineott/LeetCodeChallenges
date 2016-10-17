8c5da15814a5942d86dd94954d836e1cad19e542
Nadine Ott <nadineott7@yahoo.com>
2016-10-17 14:04:29 -0700

    Min Cost bug fix

--- minCost.js
+++ minCost.js
@@ -1,10 +1,25 @@
 var minCost = function(costs) {
+  //find all permutations of indeces with no repeats
   var houses = costs.length;
+  var min = Infinity;
   if (houses === 0){
     return 0;
   }
-  var options = [costs[0][0], costs[0][1], costs[0][2]];
-  options.sort(function(a,b){return a-b});
-  var total = Math.ceil(houses/2)*options[0] + Math.floor(houses/2)*options[1];
-  return total;
+  var permutate = function(count, total, lastIndex, str){
+    if (count === houses){
+      min = Math.min(total, min);
+    }
+    else {
+      for (var i = 0; i < 3; i++){
+        if (lastIndex !== i){
+          permutate(count+1, total+costs[count][i], i, str+costs[count][i]);
+        }
+      }
+    }
+  }
+  permutate(0, 0, 10, "");
+  return min;
 };
+
+console.log("expect 13", minCost([[20,18,4],[9,9,10]]));
+console.log("expect 26 ", minCost([[3,5,3],[6,17,6],[7,13,18],[9,10,18]]))
