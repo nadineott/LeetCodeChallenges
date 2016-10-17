70694ea74be9f66df6e47f525e51c8d5688871b8
Nadine Ott <nadineott7@yahoo.com>
2016-10-17 12:00:01 -0700

    Min Cost edited

--- minCost.js
+++ minCost.js
@@ -1,12 +1,10 @@
 var minCost = function(costs) {
-    if (costs.length === 0){
-        return 0;
-    }
-    var options = [costs[0][0], costs[1][0], costs[2][0]]
+  var houses = costs.length;
+  if (houses === 0){
+    return 0;
+  }
+  var options = [costs[0][0], costs[0][1], costs[0][2]];
   options.sort(function(a,b){return a-b});
-  var firstColor = Math.ceil(costs[0].length/2);
-  var secondColor = Math.floor(costs[0].length/2);
-  var total = firstColor*options[0] + secondColor*options[1];
+  var total = Math.ceil(houses/2)*options[0] + Math.floor(houses/2)*options[1];
   return total;
 };
-g
